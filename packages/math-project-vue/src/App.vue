a75fb95105949057d2d4ba070e90067d2b5745b9
moumou <1482283801@qq.com>
2024-09-07 19:18:48 +0800

    monorepo init

---
+++ packages/math-project-vue/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import dayjs from "dayjs";
+import { Person } from "@ruoshuisa/tools";
+
+const p = new Person("23 张雨佳", "math");
+</script>
+
+<template>
+  <div>{{ dayjs().format("YYYY年MM月DD日 HH:mm:ss") }}</div>
+  <div> {{ p.name + ':' + p.slogan }}</div>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
